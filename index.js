f737895657575e4bde24e66bd16238ff3600cabd
Justin Daniel <justinddaniel@gmail.com>
2018-01-17 04:50:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,3 +12,7 @@ function exactMatch (array, object) {
   let key = Object.keys(object)[0];
   return array.filter(function (element) {return element[key] === object[key];});
 }
+
+function exactMatchToList (array, object) {
+  return exactMatch(array, object).map(function (element) { return element['name'];});
+}
