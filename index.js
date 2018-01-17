293fbe246c68e3e4be26a8e5bd135efbfba63d15
Justin Daniel <justinddaniel@gmail.com>
2018-01-17 04:34:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,8 @@
 function driversWithRevenueOver (array, integer) {
   return array.filter(function (element) { return element['revenue'] > integer;});
 }
+
+function driverNamesWithRevenueOver (array, integer) {
+  let newarray = driversWithRevenueOver(array, integer).map(function (element) { return element['name'];});
+  return newarray
+}
